17338994200d613321b9a33a4d723a00dd93cba9
Andrew Smith <ajs3993@gmail.com>
2020-01-28 12:47:12 -0600

    received list of users from api

--- src/App.js
+++ src/App.js
@@ -6,32 +6,32 @@ class App extends Component{
   constructor(){
     super()
 
+
+
     this.state = {
-      monsters: [
-        {
-          name: 'Frankenstein'
-        },
-        {
-          name: "Dracula"
-        },
-        {
-          name: "Zombie"
-        }
-      ]
-    }
+      monsters: []
+    };
   }
 
+
+
+componentDidMount(){
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(users => this.setState({monsters: users}))
+}
+
+
+
   render(){
-  return (
-    <div className="App">
+    return (
+      <div className="App">
+        {this.state.monsters.map(monster => <h1 key = {monster.id}>  { monster.name} </h1>)}
+      </div>
+          );
+        }
 
-{
-this.state.monsters.map(monster => <h1> { monster.name} </h1>)
-}
 
-    </div>
-  );
-}
 }
 
 export default App;
