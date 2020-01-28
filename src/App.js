c328caa112abd7ad904495903fe06497fc9f5281
Andrew Smith <ajs3993@gmail.com>
2020-01-28 14:54:10 -0600

    searchbar filters monster list

--- src/App.js
+++ src/App.js
@@ -8,14 +8,15 @@ class App extends Component{
     super()
 
 
-
+//set state
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
 
-
+//get list of monsters from API
 componentDidMount(){
   fetch('https://jsonplaceholder.typicode.com/users')
   .then(response => response.json())
@@ -25,13 +26,30 @@ componentDidMount(){
 
 
   render(){
+    
+    //destructure: pulling values from state object
+    const { monsters, searchField } = this.state
+    
+    // could also be written as:
+
+    // const monsters = this.state.monsters;
+    // const searchField = this.state.searchField
+    
+const filteredMonsters = monsters.filter(monster => 
+  monster.name.toLowerCase().includes(searchField.toLowerCase())
+  )
+
     return (
       <div className="App">
-        <CardList monsters = {this.state.monsters}/>
+        <input type = 'search' placeholder='search monsters' 
+        onChange={e => this.setState({ searchField: e.target.value })
+      
+      }/>
+    {/* pass array of monsters to the cardList component  */}
+        <CardList monsters = {filteredMonsters}/>
       </div>
           );
         }
-
 
 }
 
