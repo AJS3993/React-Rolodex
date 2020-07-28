4433b0a34a73cea2ca4a96eceab6cbc0ce9286e0
Andrew Smith <ajs3993@gmail.com>
2020-07-28 16:41:25 -0500

    Updates

---
+++ precache-manifest.29b87633fbe4dfe6d501b7100e89c821.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df356c25a1954909c7ac1085eed5e7d2",
+    "url": "/React-Rolodex/index.html"
+  },
+  {
+    "revision": "496a7db379a1ba2b6299",
+    "url": "/React-Rolodex/static/css/main.fc76a4d4.chunk.css"
+  },
+  {
+    "revision": "de7be7a18f3a5ae4f5d3",
+    "url": "/React-Rolodex/static/js/2.2b498a4e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React-Rolodex/static/js/2.2b498a4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "496a7db379a1ba2b6299",
+    "url": "/React-Rolodex/static/js/main.581335e7.chunk.js"
+  },
+  {
+    "revision": "a85bea8e2d96e74a910e",
+    "url": "/React-Rolodex/static/js/runtime-main.b1a747a1.js"
+  }
+]);
